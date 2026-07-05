53c90086a5f18c15a9caf68fee045b8e7be65dce
Jan Beernink <1215417+janbeernink@users.noreply.github.com>
2026-07-05 21:49:37 +0200

    Add support for scanning JARs on the class path instead of only scanning the module path.  (#29)
    This change will cause the class path to be scanned if the module path is not set. This change also ensure the boot modules are always scanned, regardless of if the class path or module path is being used.
    
    Fixes #13 and #11

--- discovery/scanner/src/main/java/eu/jbeernink/hypospray/discovery/scanner/BeanDiscoveryExtension.java
+++ discovery/scanner/src/main/java/eu/jbeernink/hypospray/discovery/scanner/BeanDiscoveryExtension.java
@@ -63,11 +63,16 @@ public class BeanDiscoveryExtension implements BuildCompatibleExtension {
 
 	private static Stream<ModuleReference> getModules() {
 		String modulePath = System.getProperty("jdk.module.path");
-		if (modulePath != null) {
-			return getModulesFromPath(modulePath);
+		if (modulePath != null && !modulePath.isBlank()) {
+			return Stream.concat(getModulesFromPath(modulePath), getBootModules());
 		}
 
-		return ModuleLayer.boot().configuration().modules().stream().map(ResolvedModule::reference);
+		String classPath = System.getProperty("java.class.path");
+		if (classPath != null && !classPath.isBlank()) {
+			return Stream.concat(getModulesFromPath(System.getProperty("java.class.path")), getBootModules());
+		}
+
+		return getBootModules();
 	}
 
 	private Stream<String> findCandidateClasses(ModuleReference beanArchive) {
@@ -183,5 +188,9 @@ public class BeanDiscoveryExtension implements BuildCompatibleExtension {
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
+	}
+
+	private static Stream<ModuleReference> getBootModules() {
+		return ModuleLayer.boot().configuration().modules().stream().map(ResolvedModule::reference);
 	}
 }
--- discovery/scanner/src/main/java/module-info.java
+++ discovery/scanner/src/main/java/module-info.java
@@ -2,11 +2,15 @@ import jakarta.enterprise.inject.build.compatible.spi.BuildCompatibleExtension;
 
 import eu.jbeernink.hypospray.discovery.scanner.BeanDiscoveryExtension;
 import eu.jbeernink.hypospray.discovery.scanner.BeanDiscoveryFilter;
+import eu.jbeernink.hypospray.annotation.service.GenerateServiceDescriptors;
 
+@GenerateServiceDescriptors
 module eu.jbeernink.hypospray.discovery.scanner {
 	requires jakarta.cdi;
+	requires eu.jbeernink.hypospray.annotation;
 	requires eu.jbeernink.hypospray.core;
 	requires eu.jbeernink.hypospray.xml;
+	requires org.jspecify;
 
 	exports eu.jbeernink.hypospray.discovery.scanner;
 
